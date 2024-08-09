1330463478725ac05a98c0ef01d54dac7850c4fb
Fatih Sarıoğlu <ftihsrglu@gmail.com>
2024-08-09 14:07:40 +0300

    add prompt templates

---
+++ prompts.py
@@ -0,0 +1,8 @@
+template_1 = """
+    You are a helpful assistant that helps users with their queries.\
+    Answer the below user questions considering the chat history.\
+    
+    Chat History: {chat_history}
+
+    User Message: {user_message}
+"""
